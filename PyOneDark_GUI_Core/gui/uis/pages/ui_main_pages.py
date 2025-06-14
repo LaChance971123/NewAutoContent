e41aa0aa7c0b0b04269d429a634d4593712bcfa4
LaChance971123 <lachance9711@gmail.com>
2025-06-14 15:29:33 -0400

    Add startup warnings and final success message

--- PyOneDark_GUI_Core/gui/uis/pages/ui_main_pages.py
+++ PyOneDark_GUI_Core/gui/uis/pages/ui_main_pages.py
@@ -1,173 +1,35 @@
-# ///////////////////////////////////////////////////////////////
-#
-# BY: WANDERSON M.PIMENTA
-# PROJECT MADE WITH: Qt Designer and PySide6
-# V: 1.0.0
-#
-# This project can be used freely for all uses, as long as they maintain the
-# respective credits only in the Python scripts, any information in the visual
-# interface (GUI) can be modified without any implication.
-#
-# There are limitations on Qt licenses if you want to use your products
-# commercially, I recommend reading them on the official website:
-# https://doc.qt.io/qtforpython/licenses.html
-#
-# ///////////////////////////////////////////////////////////////
-
-# IMPORT QT CORE
-# ///////////////////////////////////////////////////////////////
 from qt_core import *
 
-
 class Ui_MainPages(object):
-    def setupUi(self, MainPages):
-        if not MainPages.objectName():
-            MainPages.setObjectName(u"MainPages")
-        MainPages.resize(860, 600)
-        self.main_pages_layout = QVBoxLayout(MainPages)
-        self.main_pages_layout.setSpacing(0)
-        self.main_pages_layout.setObjectName(u"main_pages_layout")
-        self.main_pages_layout.setContentsMargins(5, 5, 5, 5)
-        self.pages = QStackedWidget(MainPages)
-        self.pages.setObjectName(u"pages")
-        self.page_1 = QWidget()
-        self.page_1.setObjectName(u"page_1")
-        self.page_1.setStyleSheet(u"font-size: 14pt")
-        self.page_1_layout = QHBoxLayout(self.page_1)
-        self.page_1_layout.setSpacing(5)
-        self.page_1_layout.setObjectName(u"page_1_layout")
-        self.page_1_layout.setContentsMargins(5, 5, 5, 5)
-        self.controls_scroll = QScrollArea(self.page_1)
-        self.controls_scroll.setObjectName(u"controls_scroll")
-        self.controls_scroll.setFrameShape(QFrame.NoFrame)
-        self.controls_scroll.setWidgetResizable(True)
-        self.controls_scroll.setAlignment(Qt.AlignHCenter)
+    def setupUi(self, parent):
+        if not parent.objectName():
+            parent.setObjectName("MainPages")
+        self.main_layout = QVBoxLayout(parent)
+        self.main_layout.setContentsMargins(5, 5, 5, 5)
+        self.main_layout.setSpacing(0)
+        self.pages = QStackedWidget()
+        self.main_layout.addWidget(self.pages)
+        self._widgets = {}
+        self._anim = None
 
-        self.controls_widget = QWidget()
-        self.controls_widget.setObjectName(u"controls_widget")
-        self.controls_widget.setMaximumWidth(640)
-        self.controls_layout = QVBoxLayout(self.controls_widget)
-        self.controls_layout.setSpacing(10)
-        self.controls_layout.setObjectName(u"controls_layout")
-        self.controls_layout.setContentsMargins(5, 5, 5, 5)
-        self.controls_layout.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
-        self.controls_scroll.setWidget(self.controls_widget)
-
-        self.page_1_layout.addWidget(self.controls_scroll)
-
-        self.preview_frame = QFrame(self.page_1)
-        self.preview_frame.setObjectName(u"preview_frame")
-        self.preview_frame.setFixedWidth(300)
-        self.preview_layout = QVBoxLayout(self.preview_frame)
-        self.preview_layout.setContentsMargins(5, 5, 5, 5)
-        self.preview_layout.setSpacing(0)
-        self.page_1_layout.addWidget(self.preview_frame, 0, Qt.AlignTop)
-
-        self.page_1_layout.setStretch(0, 7)
-        self.page_1_layout.setStretch(1, 3)
-
-        self.pages.addWidget(self.page_1)
-        self.page_2 = QWidget()
-        self.page_2.setObjectName(u"page_2")
-        self.page_2_layout = QVBoxLayout(self.page_2)
-        self.page_2_layout.setSpacing(5)
-        self.page_2_layout.setObjectName(u"page_2_layout")
-        self.page_2_layout.setContentsMargins(5, 5, 5, 5)
-        self.scroll_area = QScrollArea(self.page_2)
-        self.scroll_area.setObjectName(u"scroll_area")
-        self.scroll_area.setStyleSheet(u"background: transparent;")
-        self.scroll_area.setFrameShape(QFrame.NoFrame)
-        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.scroll_area.setWidgetResizable(True)
-        self.contents = QWidget()
-        self.contents.setObjectName(u"contents")
-        self.contents.setGeometry(QRect(0, 0, 840, 580))
-        self.contents.setStyleSheet(u"background: transparent;")
-        self.verticalLayout = QVBoxLayout(self.contents)
-        self.verticalLayout.setSpacing(15)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(5, 5, 5, 5)
-        self.title_label = QLabel(self.contents)
-        self.title_label.setObjectName(u"title_label")
-        self.title_label.setMaximumSize(QSize(16777215, 40))
-        font = QFont()
-        font.setPointSize(16)
-        self.title_label.setFont(font)
-        self.title_label.setStyleSheet(u"font-size: 16pt")
-        self.title_label.setAlignment(Qt.AlignCenter)
-
-        self.verticalLayout.addWidget(self.title_label)
-
-        self.description_label = QLabel(self.contents)
-        self.description_label.setObjectName(u"description_label")
-        self.description_label.setAlignment(Qt.AlignHCenter|Qt.AlignTop)
-        self.description_label.setWordWrap(True)
-
-        self.verticalLayout.addWidget(self.description_label)
-
-        self.row_1_layout = QHBoxLayout()
-        self.row_1_layout.setObjectName(u"row_1_layout")
-
-        self.verticalLayout.addLayout(self.row_1_layout)
-
-        self.row_2_layout = QHBoxLayout()
-        self.row_2_layout.setObjectName(u"row_2_layout")
-
-        self.verticalLayout.addLayout(self.row_2_layout)
-
-        self.row_3_layout = QHBoxLayout()
-        self.row_3_layout.setObjectName(u"row_3_layout")
-
-        self.verticalLayout.addLayout(self.row_3_layout)
-
-        self.row_4_layout = QVBoxLayout()
-        self.row_4_layout.setObjectName(u"row_4_layout")
-
-        self.verticalLayout.addLayout(self.row_4_layout)
-
-        self.row_5_layout = QVBoxLayout()
-        self.row_5_layout.setObjectName(u"row_5_layout")
-
-        self.verticalLayout.addLayout(self.row_5_layout)
-
-        self.scroll_area.setWidget(self.contents)
-
-        self.page_2_layout.addWidget(self.scroll_area)
-
-        self.pages.addWidget(self.page_2)
-        self.page_3 = QWidget()
-        self.page_3.setObjectName(u"page_3")
-        self.page_3.setStyleSheet(u"QFrame {\n"
-"	font-size: 16pt;\n"
-"}")
-        self.page_3_layout = QVBoxLayout(self.page_3)
-        self.page_3_layout.setObjectName(u"page_3_layout")
-        self.empty_page_label = QLabel(self.page_3)
-        self.empty_page_label.setObjectName(u"empty_page_label")
-        self.empty_page_label.setFont(font)
-        self.empty_page_label.setAlignment(Qt.AlignCenter)
-
-        self.page_3_layout.addWidget(self.empty_page_label)
-
-        self.pages.addWidget(self.page_3)
-
-        self.main_pages_layout.addWidget(self.pages)
-
-
-        self.retranslateUi(MainPages)
-
+    def load_pages(self, mapping: dict):
+        for name, cls in mapping.items():
+            widget = cls()
+            self.pages.addWidget(widget)
+            self._widgets[name] = widget
         self.pages.setCurrentIndex(0)
 
-
-        QMetaObject.connectSlotsByName(MainPages)
-    # setupUi
-
-    def retranslateUi(self, MainPages):
-        MainPages.setWindowTitle(QCoreApplication.translate("MainPages", u"Form", None))
-        self.title_label.setText(QCoreApplication.translate("MainPages", u"Custom Widgets Page", None))
-        self.description_label.setText(QCoreApplication.translate("MainPages", u"Here will be all the custom widgets, they will be added over time on this page.\n"
-"I will try to always record a new tutorial when adding a new Widget and updating the project on Patreon before launching on GitHub and GitHub after the public release.", None))
-        self.empty_page_label.setText(QCoreApplication.translate("MainPages", u"Empty Page", None))
-    # retranslateUi
+    def set_current(self, name: str):
+        widget = self._widgets.get(name)
+        if widget is not None:
+            self.pages.setCurrentWidget(widget)
+            effect = QGraphicsOpacityEffect(widget)
+            widget.setGraphicsEffect(effect)
+            anim = QPropertyAnimation(effect, b"opacity")
+            anim.setDuration(150)
+            anim.setStartValue(0)
+            anim.setEndValue(1)
+            anim.finished.connect(lambda: widget.setGraphicsEffect(None))
+            anim.start(QPropertyAnimation.DeleteWhenStopped)
+            self._anim = anim
 
